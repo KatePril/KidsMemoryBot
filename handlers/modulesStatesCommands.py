5f13de4d3e0c54573d399b07fab9bb6d07804a88
KatePril <ket14pril14@gmail.com>
2023-05-02 21:03:21 +0300

    On branch master Your branch is up to date with 'origin/master'.

--- handlers/modulesStatesCommands.py
+++ handlers/modulesStatesCommands.py
@@ -5,7 +5,8 @@ from loader import dp
 from utils_data.modules_data import *
 from utils_data.module_body import open_photo, generate_answers_markup
 from utils import update_changes, save_users
-from utils_data.module_buttons import update_state_data, finish_module
+from utils_data.module_buttons import *
+from utils_data.grades_data import WRONG_ANSWER_STICKER, CORRECT_ANSWER_STICKER
 
 from states.playFrom import PlayFrom
 from aiogram.dispatcher import FSMContext
@@ -29,17 +30,15 @@ async def start_module(call: types.CallbackQuery, state: FSMContext):
     await state.update_data(current_question = data['current_question'] + 1)
     await PlayFrom.next()
 
-@dp.callback_query_handler(text_contains='wrong_', state=PlayFrom.current_question)
+@dp.callback_query_handler(text='wrong_answer', state=PlayFrom.current_question)
 async def reply_wrong_answer(call: types.CallbackQuery, state: FSMContext):
     data = await state.get_data()
     if data['current_question'] == 11:
         await finish_module(state, call.message, data)
     else:
-        await call.message.answer_sticker('CAACAgIAAxkBAAIBAWQ8Lj_VnjsQBO3CtuhH03sgUAeMAAJaAgACVp29ClvFwUJa4dm6LwQ')
-        await call.message.answer_photo(open_photo(data['current_module'][data['current_question'] - 1]['path']), reply_markup=generate_answers_markup(data['current_module'], data['current_module'][data['current_question'] - 1]['name']))
-        await state.update_data(current_question = data['current_question'] + 1)
+        await next_question(state, call.message, data, WRONG_ANSWER_STICKER)
 
-@dp.callback_query_handler(text_contains='correct_', state=PlayFrom.current_question)
+@dp.callback_query_handler(text='correct_answer', state=PlayFrom.current_question)
 async def reply_correct_answer(call: types.CallbackQuery, state: FSMContext):
     data = await state.get_data()
     await state.update_data(current_points = data['current_points'] + 1)
@@ -47,7 +46,5 @@ async def reply_correct_answer(call: types.CallbackQuery, state: FSMContext):
     if data['current_question'] == 11:
         await finish_module(state, call.message, data)
     else:
-        await call.message.answer_sticker('CAACAgIAAxkBAAPuZDvwj55ADV4_8jN97YFR-utbR0oAAiwAA_cCyA-8QeDI--FA0y8E')
-        await call.message.answer_photo(open_photo(data['current_module'][data['current_question'] - 1]['path']), reply_markup=generate_answers_markup(data['current_module'], data['current_module'][data['current_question'] - 1]['name']))
-        await state.update_data(current_question = data['current_question'] + 1)
+        await next_question(state, call.message, data, CORRECT_ANSWER_STICKER)
 
--- utils_data/module_buttons.py
+++ utils_data/module_buttons.py
@@ -1,7 +1,8 @@
 from random import shuffle
 from keyboard.modules_kb import generate_again_kb
 from keyboard.menu_kb import menu_kb
-from utils_data.grades_data import grades_stickers
+from utils_data.grades_data import GRADES_STICKERS 
+from utils_data.module_body import open_photo, generate_answers_markup
 
 from states.MenuForm import MenuForm
 from states.playFrom import PlayFrom
@@ -12,7 +13,7 @@ async def update_state_data(state, module, name):
 
 async def finish_module(state, call, data):
     await call.answer(f'It is the end of the module. Your points are {data["current_points"] + 1}')
-    await call.answer_sticker(grades_stickers[data["current_points"] + 1])
+    await call.answer_sticker(GRADES_STICKERS[data["current_points"] + 1])
     if data["current_points"] > 4:
         await state.finish()
         await call.answer('Congradulations🎊🎉. Now, you can move forward', reply_markup=menu_kb)
@@ -22,3 +23,8 @@ async def finish_module(state, call, data):
         await state.finish()
         await call.answer('Try again😢', reply_markup=generate_again_kb(data['module_name']))
         await PlayFrom.module_name.set()
+
+async def next_question(state, call, data, sticker):
+    await call.answer_sticker(sticker)
+    await call.answer_photo(open_photo(data['current_module'][data['current_question'] - 1]['path']), reply_markup=generate_answers_markup(data['current_module'], data['current_module'][data['current_question'] - 1]['name']))
+    await state.update_data(current_question = data['current_question'] + 1)
